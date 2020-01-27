b1ef7a95fcadee9a99d717ebe641fb0a9327d6b4
laavang <laavang@gmail.com>
2020-01-26 18:53:17 -0600

    Built simple pig latin GUI

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
-'use strict';
+// 'use strict';
 
-// brings in the assert module for unit testing
-const assert = require('assert');
-// brings in the readline module to access the command line
-const readline = require('readline');
-// use the readline module to print out to the command line
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+// // brings in the assert module for unit testing
+// const assert = require('assert');
+// // brings in the readline module to access the command line
+// const readline = require('readline');
+// // use the readline module to print out to the command line
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
 
 
+const pigLatin = () => {
 
-const pigLatin = (word) => {
+  let word = document.getElementById("inputToTranslate").value;
 
   //array of vowels to check against
   const vowels = ["a","e","i","o","u"];
@@ -35,7 +36,7 @@ const pigLatin = (word) => {
   if (startsWithVowel == true) {
     word += "yay";
     console.log(word);
-    return word;
+    document.getElementById("translation").innerHTML = word;
   }
   //if word doesn't start with a vowel
   else {
@@ -57,63 +58,16 @@ const pigLatin = (word) => {
     // add 'ay' to end of word
     word += "ay";
     console.log(word);
-    return word;
+    document.getElementById("translation").innerHTML = word;
 }
-}
-
-
-// the first function called in the program to get an input from the user
-// to run the function use the command: node main.js
-// to close it ctrl + C
-const getPrompt = () => {
-  rl.question('word ', (answer) => {
-    console.log( pigLatin(answer) );
-    getPrompt();
-  });
+  
 }
 
-// Unit Tests
-// You use them run the command: npm test main.js
-// to close them ctrl + C
-if (typeof describe === 'function') {
-
-  describe('#pigLatin()', () => {
-    it('should translate a simple word', () => {
-      assert.equal(pigLatin('car'), 'arcay');
-      assert.equal(pigLatin('dog'), 'ogday');
-    });
-    it('should translate a complex word', () => {
-      assert.equal(pigLatin('create'), 'eatecray');
-      assert.equal(pigLatin('valley'), 'alleyvay');
-    });
-    it('should attach "yay" if word begins with vowel', () => {
-      assert.equal(pigLatin('egg'), 'eggyay');
-      assert.equal(pigLatin('emission'), 'emissionyay');
-    });
-    it('should lowercase and trim word before translation', () => {
-      assert.equal(pigLatin('HeLlO '), 'ellohay');
-      assert.equal(pigLatin(' RoCkEt'), 'ocketray');
-    });
-    it('Should separate two words and return them together', () => {
-      assert.equal(pigLatin('Hop Fest '), 'Ophay Estfay');
-    });
-  });
-} else {
-
-  getPrompt();
-
-}
-
-
-
-
-
-
-// **********
-//   HINTS
-// **********
-
-// break your code into pieces and focus on one piece at a time...
-// 1. if word begins with a vowel send to one function: adds "yay"
-// 2. if word begins in with a consonant send to another function: splices off beginning, returns word with new ending.
-// 3. if multiple words, create array of words, loop over them, sending them to different functions and creating a new array with the new words.
+// // the first function called in the program to get an input from the user
+// // to run the function use the command: node main.js
+// // to close it ctrl + C
+// const getPrompt = () => {
+//   rl.question('word ', (answer) => {
+//     console.log( pigLatin(answer) );
+//     getPrompt();
+//   });
